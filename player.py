3b2aa0e5040aa48bd1a726185c80cf5704f47eb5
le Monke <joeks05@icloud.com>
2021-08-01 13:19:09 -0500

    fixed view inventory action

--- player.py
+++ player.py
@@ -22,7 +22,7 @@ class Player():
  
     def print_inventory(self):
         for item in self.inventory:
-            print(item, '\n')
+            print(item.name, '\n')
 
     def move(self, dx, dy):
         self.location_x += dx
